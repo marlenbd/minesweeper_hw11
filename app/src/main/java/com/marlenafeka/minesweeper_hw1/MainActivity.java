8c5d7f8c25aaf557d454de58042ba4b4e6e523f6
marlen <Jiyuksbu18>
2017-08-25 11:06:56 +0300

    timer is still problematic and missing record

--- app/src/main/java/com/marlenafeka/minesweeper_hw1/MainActivity.java
+++ app/src/main/java/com/marlenafeka/minesweeper_hw1/MainActivity.java
@@ -13,12 +13,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro_activity);
 
-        GameActivity.time = 0;
+        //GameActivity.time = 0;
 
         Button buttonBeginner = (Button)findViewById(R.id.buttonBeginner);
         Button buttonSkilled = (Button)findViewById(R.id.buttonSkilled);
         Button buttonExpert = (Button)findViewById(R.id.buttonExpert);
-        //Button buttonAbout = (Button)findViewById(R.id.buttonAbout);
 
         buttonBeginner.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,14 +45,5 @@ public class MainActivity extends Activity {
                 startActivity(intentExpert);
             }
         });
-
-        /*buttonAbout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /// about ///
-                Intent intent = new Intent(MainActivity.this, EndGameActivity.class);
-                startActivity(intent);
-            }
-        });*/
     }
 }
