b5ed2ebf7ae5345b979e69d13c636ddae09e6e49
Perry Shalom <perry@nested-group.com>
2017-09-06 07:50:15 +0300

    Avoiding code duplications

--- app/src/main/java/com/marlenafeka/minesweeper_hw1/GameActivity.java
+++ app/src/main/java/com/marlenafeka/minesweeper_hw1/GameActivity.java
@@ -1,20 +1,13 @@
 package com.marlenafeka.minesweeper_hw1;
 
-import android.nfc.Tag;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-import java.text.NumberFormat;
 import java.text.ParseException;
 
 public class GameActivity extends AppCompatActivity {
@@ -34,22 +27,19 @@ public class GameActivity extends AppCompatActivity {
             case "beginner":
                 myBombsText = "5 bombs";
                 GameEngine.setLevel(5, 10 ,10);
-                GameEngine.getInstance().setRunning(true);
-                GameEngine.getInstance().createGrid(this);
                 break;
             case "skilled":
                 myBombsText = "10 bombs";
                 GameEngine.setLevel(10, 10 ,10);
-                GameEngine.getInstance().setRunning(true);
-                GameEngine.getInstance().createGrid(this);
                 break;
             case "expert":
                 myBombsText = "10 bombs";
                 GameEngine.setLevel(10, 5 ,5);
-                GameEngine.getInstance().setRunning(true);
-                GameEngine.getInstance().createGrid(this);
                 break;
         }
+
+        GameEngine.getInstance().setRunning(true);
+        GameEngine.getInstance().createGrid(this);
 
         setContentView(R.layout.activity_game);
         TextView myBombs = (( TextView)findViewById(R.id.textBombs));
